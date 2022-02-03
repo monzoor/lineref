34f389fd09625723f41f1f4b66a72e478dd1f6db
monzoor-rm <monzoor.morshed@raremark.com>
2022-02-04 04:03:51 +0600

    Add search

--- src/containers/Home/List.tsx
+++ src/containers/Home/List.tsx
@@ -1,15 +1,59 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 import { LS_KEYS } from '@constants';
 import { getLSValue } from '@utils/storage';
 import TableHeader from './TableHeader';
+import { Form, Input } from '@components';
+import { FIELDS } from '@constants/fields';
+import { useAppState } from '@context/Provider';
 
 const Lists: FC = () => {
-  const itemList = getLSValue(LS_KEYS.USER_DATA);
+  const [itemList, setListItems] = useState(getLSValue(LS_KEYS.USER_DATA));
+  const [state] = useAppState();
+  // const itemList = getLSValue(LS_KEYS.USER_DATA);
+
+  const methods = useForm({ mode: 'onChange' });
+  const { watch, setValue } = methods;
+
+  useEffect(() => {
+    setListItems(getLSValue(LS_KEYS.USER_DATA));
+    setValue(FIELDS.SEARCH, '');
+  }, [state]);
+
+  useEffect(() => {
+    const subscription = watch((value) => {
+      if (value.search === '') {
+        setListItems(getLSValue(LS_KEYS.USER_DATA));
+      } else {
+        const searchItems = itemList.filter((item: any) => {
+          return item.name.toLowerCase().includes(value.search.toLowerCase());
+        });
+        setListItems(searchItems);
+      }
+    });
+
+    return () => subscription.unsubscribe();
+  }, [watch, itemList]);
 
   return (
     <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div className="flex justify-end w-full mb-5">
+          <Form methods={methods}>
+            {(props: any) => (
+              <Input
+                {...props}
+                component="input"
+                type="text"
+                placeholder="Search by name"
+                id={FIELDS.SEARCH}
+                name={FIELDS.SEARCH}
+                className="w-full"
+              />
+            )}
+          </Form>
+        </div>
         <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
           <table className="min-w-full divide-y divide-gray-200">
             <TableHeader />
---
+++ src/containers/Home/Search.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Search = () => {
+  return (
+    <div>
+      <p>asdasd</p>
+    </div>
+  );
+};
+
+export default Search;
