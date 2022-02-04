ed1046fa1787146d9a73cbaf2e46feeb9aaa104f
monzoor-rm <monzoor.morshed@raremark.com>
2022-02-05 03:28:50 +0600

    Add sentry logger

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./containers/Core";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 
-import reportWebVitals from "./reportWebVitals";
+import './index.css';
+import App from './containers/Core';
+
+import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: 'https://c21cf2059d2143d694cb65790bf0a0f6@o1135733.ingest.sentry.io/6186017',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
